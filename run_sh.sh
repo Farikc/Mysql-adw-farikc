aa62689061569dcfe9cb7e01887cf2007a32fbfa
Farikc Palacios <farikc.palacios@udea.edu.co>
2023-09-28 04:39:54 +0000

    queries tareas

---
+++ run_sh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./detener-contenedor.sh
+./ejecutar-contenedor.sh
+./ejecutar-mysql.sh
--- scripts/sales.sql
+++ scripts/sales.sql
@@ -52,3 +52,29 @@ group by year(SOH.OrderDate), month(SOH.OrderDate), SPT.Name
 select *
 from sales_per_month
 limit 10;
+
+
+
+with sales_per_date as (
+    select 
+        year(SOH.OrderDate) order_year,
+        month(SOH.OrderDate) order_month,
+        SOD.SalesOrderId,
+        SOD.ProductID,
+        SOD.LineTotal,
+        PDT.ProductSubcategoryId,
+        PDT.Name SubCatName,
+        PSC.ProductCategoryId,
+        PSC.Name CatName
+    from    Sales_SalesOrderDetail SOD join
+    Sales_SalesOrderHeader SOH using(SalesOrderId) join
+    Production_Product PDT using(ProductID) join
+    Production_ProductSubcategory PSC using(ProductSubcategoryId)
+),
+sales_per_date_per_subcat(
+    select *
+    from sales_per_date
+)
+select *
+from sales_per_date_per_subcat
+limit 5;
